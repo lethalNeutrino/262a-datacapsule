3516ae12d66e909d92a79eb3a87dceb88461594d
Andrew <liu.andrew@berkeley.edu>
2025-12-04 01:58:35 -0800

    feat: init project structure

---
+++ userlib/lib.rs
@@ -0,0 +1,21 @@
+use anyhow::Result;
+use std::collections::HashMap;
+
+pub fn create() -> Result<()> {
+    Ok(())
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
