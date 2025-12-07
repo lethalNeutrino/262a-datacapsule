f9f96b400f54745318c9046ba65e973c1b71c116
Andrew <liu.andrew@berkeley.edu>
2025-12-07 07:32:54 +0000

    feat(server): add basic echo server

--- server/main.rs
+++
@@ -1,6 +0,0 @@
-#[tokio::main]
-async fn main() {
-    println!("Starting tokio runtime...");
-    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-    println!("Tokio async main finished.");
-}
