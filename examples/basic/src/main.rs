6ab528af1d189a8d43c40e04ffc9607872895b2a
Andrew Liu <liu.andrew@berkeley.edu>
2025-12-08 18:30:09 +0000

    feat: update example to use signatures

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -2,22 +2,46 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate log;
 use anyhow::bail;
+use capsulelib::capsule::structs::{Capsule, Metadata};
 use ed25519_dalek::SigningKey;
 use std::{collections::BTreeMap, path::Path};
-use capsulelib::{Capsule, Metadata};
 
 fn main() -> anyhow::Result<()> {
     pretty_env_logger::init();
     let data_path = ".fjall_data";
-    let metadata: BTreeMap<String, Vec<u8>> =
-        BTreeMap::from([(String::from("verify_key"), (0..16).collect::<Vec<u8>>())]);
 
     let signing_key_seed = [0u8; 32];
-    let signing_key = SigningKey::from_bytes(&signing_key_seed);
+    let generated_signing_key = SigningKey::from_bytes(&signing_key_seed);
+    let generated_verify_key = generated_signing_key.verifying_key();
+    let generated_verify_key_bytes = generated_verify_key.to_bytes();
+
+    info!(
+        "Generated signing key seed: {}",
+        signing_key_seed
+            .iter()
+            .map(|b| format!("{:02x}", b))
+            .collect::<String>()
+    );
+    info!(
+        "Generated verify key: {}",
+        generated_verify_key_bytes
+            .iter()
+            .map(|b| format!("{:02x}", b))
+            .collect::<String>()
+    );
+    let metadata: BTreeMap<String, Vec<u8>> = BTreeMap::from([(
+        String::from("verify_key"),
+        generated_verify_key_bytes.to_vec(),
+    )]);
 
     let encryption_key = (0..16).collect::<Vec<u8>>();
 
-    let mut capsule = Capsule::create(data_path, Metadata(metadata), signing_key, encryption_key)?;
+    let mut capsule = Capsule::create(
+        data_path,
+        Metadata(metadata),
+        generated_signing_key,
+        encryption_key,
+    )?;
     info!("Capsule created successfully");
 
     let data = "Hello, world!";
